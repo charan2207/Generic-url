d1d45b6ae2985d20c2da5b2eb88a81411924274f
charan2207 <arisettycharan12@gmail.com>
2023-03-23 17:23:06 +0530

    first commit

---
+++ csk/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+def hari(request):
+    return render(request,'hari.html')
+
+
+def charan(request):
+    return render(request,'charan.html')
+
